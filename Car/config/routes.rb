737d33fec3a3e34c532620eb6f4c249a7400b4f1
think <think@ubuntu.ubuntu-domain>
2015-06-09 22:42:40 +0800

    add

--- Car/config/routes.rb
+++ Car/config/routes.rb
@@ -55,4 +55,5 @@ Car::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+   match ':controller(/:id)/:action'
 end

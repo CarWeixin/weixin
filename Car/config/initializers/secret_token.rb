1fd2c41c07e881ba5e140ca3719cecc960309aa5
think <think@ubuntu.ubuntu-domain>
2015-06-09 19:04:23 +0800

    new file

---
+++ Car/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Car::Application.config.secret_token = 'beba049e2d032b7c5fce0c23a14a2c7a70d34c63d95420cc195e3d49449ce2c7dc43f94d34b46d5453a4e9549c7a38b56b96770966d21842e4521c90593da1b1'

82f59469545a1553e1a3d5845b6a501706c01819
think <think@ubuntu.ubuntu-domain>
2015-06-09 22:40:10 +0800

    add

---
+++ Car/app/controllers/tokens_controller.rb
@@ -0,0 +1,27 @@
+class TokensController < ApplicationController
+
+	def get
+			sign = params[ :signature]
+			time = params[ :timestamp]
+			nonce = params[ :nonce]
+			echostr = params[ :echostr]
+
+			token = "huyindianzishangwu"
+
+			array = [token , time , nonce]
+
+			array = array.sort 
+
+			str = array[0] + array[1] + array[2]
+
+			str = Digest::SHA1.hexdigest(str)
+
+
+			#render(:text => str)
+		
+			if str == sign 
+			    render(:text => echostr)   
+				return true
+			end
+	end
+end
